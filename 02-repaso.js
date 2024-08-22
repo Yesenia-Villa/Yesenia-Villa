7e80e3c4ca0aae7a7d3823dfb4ceee216508e2ee
yesenia villa <mateoyesenia781@gmail.com>
2024-08-22 16:51:51 -0600

    actividad 1

---
+++ 02-repaso.js
@@ -0,0 +1,20 @@
+/*/Realizar un arreglo con 2 dimensiones y guardar dos arreglos 
+internos y dar lectura a los datos internos*/
+
+array = [[1,2],[3,4]]
+
+const datosarreglo=()=>{
+
+    A.map((datos,index)=>{
+       datos.map((valores,i)=>{
+        console.log(`${i}=${valores}`);
+        })  
+     });
+     } 
+
+
+//for of
+//for in 
+//fuction flecha
+//while
+//for each
