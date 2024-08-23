45893dcfdec1cb8f748a36900340bd3a93ba5b3d
yesenia villa <mateoyesenia781@gmail.com>
2024-08-22 19:40:25 -0600

    envio de mis archivos con nuevos ejemplos

--- 02-repaso.js
+++ 02-repaso.js
@@ -1,20 +1,72 @@
 /*/Realizar un arreglo con 2 dimensiones y guardar dos arreglos 
 internos y dar lectura a los datos internos*/
 
-array = [[1,2],[3,4]]
 
-const datosarreglo=()=>{
-
-    A.map((datos,index)=>{
-       datos.map((valores,i)=>{
+ array= [[1,2],[3,4]];
+/* 
+    const datosArreglo= ()=>{
+      array.map((datos)=>{
+         datos.map((valores, i)=>{
         console.log(`${i}=${valores}`);
         })  
      });
-     } 
+    };
 
+     datosArreglo();
+*/
+/*for of
 
-//for of
+const  datosArreglo = () => {
+    for (const datos of array) {
+        for (const valores of datos) {
+            console.log(`${datos.indexOf(valores)}=${valores}`);
+        }
+    }
+ };
+ 
+ datosArreglo();
+ */
+
 //for in 
+/*const datosArreglo = () => {
+    for (const datosIndex in array){
+        for (const valoresIndex in array[datosIndex]) {
+            console.log(`${valoresIndex}=${array[datosIndex][valoresIndex]}`);
+        }
+    }
+};
+datosArreglo();
+*/
+
 //fuction flecha
+/*const datosArreglo =() => {
+    for (const datosIndex in array){
+        for (const valoresIndex in array [datosIndex])
+            console.log(`${valoresIndex}=${array[datosIndex][valoresIndex]}`);
+    }
+}
+datosArreglo();
+*/
 //while
+/*const datosArreglo = () =>{
+    let datosIndex = 0;
+    while(datosIndex< array.length){
+        let valoresIndex =0;
+     while (valoresIndex<array[datosIndex].length){
+        console.log(`${valoresIndex}=${array[datosIndex][valoresIndex]}`);
+        valoresIndex++;
+     }
+     datosIndex++;
+    }
+}
+datosArreglo();
+*/
 //for each
+const datosArreglo= () => {
+    array.forEach((subArray, datosIndex)=> {
+    subArray.forEach((valor, valoresIndex)=>{
+        console.log(`${valoresIndex}=${valor}`);
+    });
+});
+}
+datosArreglo();
